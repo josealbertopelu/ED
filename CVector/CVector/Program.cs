1587034d605d0a2704b9a54c52da0e599b5278cc
josealbertopelu <1dam.jcamara@institutoserpis.org>
2018-11-29 11:22:07 +0100

    Subido CVector

---
+++ CVector/CVector/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CVector
+{
+    class Vector
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int BinarySearch(int[] v, int x){
+           
+            int ini = 0;
+            int fin = v.Length - 1;
+            int mitad = (ini + fin)/2;
+            while (ini <= fin && v[mitad] != x)
+            {
+                if (x < v[mitad - 1])
+                {
+                    fin = mitad - 1;
+                }
+                else
+                {
+                    ini = mitad + 1;
+                }
+                    mitad = (ini + fin) / 2;
+
+            }
+            return v[mitad];
+        }
+    }
+}
---
+++ CVector/CVector/VectorTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+namespace CVector
+{
+    [TestFixture()]
+    public class VectorTest
+    {
+        [Test()]
+        public void BinarySearch()
+        {
+
+            Assert.AreEqual(-1, Vector.BinarySearch(new int[] { 45 }, 33));
+            Assert.AreEqual(3, Vector.BinarySearch(new int[] {2,3,4,5,6,7,10 },5));
+            Assert.AreEqual(0, Vector.BinarySearch(new int[] {-2,3,7,8,12,65 },-2));
+            Assert.AreEqual(5, Vector.BinarySearch(new int[] {-2,3,7,8,12,65 },65));
+        }
+    }
+}
