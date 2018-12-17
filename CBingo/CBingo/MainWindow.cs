b25f15ab70c304fa9711d46cc974e21a2b86f035
josealbertopelu <1dam.jcamara@institutoserpis.org>
2018-12-17 10:28:55 +0100

    Modificado la tabla y creado CBingo

---
+++ CBingo/CBingo/MainWindow.cs
@@ -0,0 +1,49 @@
+﻿using Gtk;
+using System;
+using System.Collections.Generic;
+
+public partial class MainWindow : Gtk.Window
+
+{
+    private static readonly Gdk.Color COLOR_GREEN = new Gdk.Color(0, 255, 0);
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+
+        Build();
+
+        //Button[] buttons = new Button[90];
+        List<Button> buttons = new List<Button>();
+        
+
+        for (int row = 0; row < 9; row++)
+            for (int column = 0; column < 10; column++){
+
+                Button button = new Button();
+
+                    button.Label = "66";
+                table4.Attach(button, (uint)column, (uint)column + 1, (uint)row, (uint)row + 1);
+                buttons.Add(button);
+            }
+
+        table4.ShowAll();
+
+      
+
+
+        Random random = new Random();
+        Siguiente.Clicked += delegate{
+            int numAleatorio = random.Next(90) + 1;
+            Numero.Text = numAleatorio.ToString();
+            buttons[numAleatorio - 1].ModifyBg(StateType.Normal, COLOR_GREEN);
+
+
+        };
+
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
--- CGtkTable/CGtkTable/MainWindow.cs
+++ CGtkTable/CGtkTable/MainWindow.cs
@@ -6,6 +6,19 @@ public partial class MainWindow : Gtk.Window
     public MainWindow() : base(Gtk.WindowType.Toplevel)
     {
         Build();
+        Table table = new Table(3, 3, true);
+        for (int row = 0; row < 3; row++)
+            for (int column = 0; column < 3; column++)
+            {
+                Button button = new Button();
+                table.Attach(button, (uint)column, (uint)column + 1, (uint)row, (uint)row + 1);
+                button.Clicked += delegate {
+                    button.Label = "X";
+                };
+            }
+        vBox.Add(table);
+        table.ShowAll();
+
     }
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
@@ -13,4 +26,10 @@ public partial class MainWindow : Gtk.Window
         Application.Quit();
         a.RetVal = true;
     }
+
+    //protected void OnButtonClicked(object sender, EventArgs e)
+    //{
+    //  ((Button)sender).Label = "X";
+    //  //button_el_que_sea.Label = "X";
+    //}
 }
