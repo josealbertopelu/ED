0ec36e8bc37d2d8939d2174eb72f63d3f95deb87
josealbertopelu <1dam.jcamara@institutoserpis.org>
2019-02-07 12:20:33 +0100

    modificado ASuma

--- ASuma/app/src/main/java/org/institusoserpis/ed/asuma/MainActivity.java
+++ ASuma/app/src/main/java/org/institusoserpis/ed/asuma/MainActivity.java
@@ -2,6 +2,8 @@ package org.institusoserpis.ed.asuma;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewConfiguration;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+
+    public void onButtonSumaClick(View view) {
+
     }
 }
