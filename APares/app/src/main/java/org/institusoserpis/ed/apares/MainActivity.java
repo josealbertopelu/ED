9b950b77795e069e4e9970a57ca5d6655146addb
josealbertopelu <1dam.jcamara@institutoserpis.org>
2019-02-28 12:12:44 +0100

    añadido las imagenes a los botones

--- APares/app/src/main/java/org/institusoserpis/ed/apares/MainActivity.java
+++ APares/app/src/main/java/org/institusoserpis/ed/apares/MainActivity.java
@@ -7,28 +7,24 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.imageButton);
+//        button = findViewById(R.id.imageButton);
 
 
 
     }
 
 
-    public void onButtonClick(View view){
 
-        Log.i(MainActivity.class.getSimpleName(), "Ejecutando onClickButton");
-        button
-    }
 
 //    private void setContentView(int activity_main) {
 //    }
 
-    button.postDelayed( ())
+
 
 }
