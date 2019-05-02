ece8882c1686c93b9f83f91dd7151a5c00e3934f
josealbertopelu <1dam.jcamara@institutoserpis.org>
2019-05-02 12:14:56 +0200

    Pcsharp version inicial a modo boceto

---
+++ Pcsharp/Pcsharp/MainWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        treeView.AppendColumn("Nombre", new CellRendererText(), "text", 0);
+
+        treeView.AppendColumn("Telefono", new CellRendererText(), "text", 1);
+        ListStore listStore = new ListStore(typeof(String), typeof(String));
+        treeView.Model = listStore;
+
+        listStore.AppendValues("Rosa", "666777555");
+        listStore.AppendValues("Pepe", "666757555");
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
