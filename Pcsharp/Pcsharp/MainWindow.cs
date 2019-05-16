5866c41d768a3c6eca77a9f106d15834920e9fdf
josealbertopelu <1dam.jcamara@institutoserpis.org>
2019-05-16 12:14:59 +0200

    casi terminada la agenda

--- Pcsharp/Pcsharp/MainWindow.cs
+++ Pcsharp/Pcsharp/MainWindow.cs
@@ -7,14 +7,32 @@ public partial class MainWindow : Gtk.Window
     {
         Build();
         treeView.AppendColumn("Nombre", new CellRendererText(), "text", 0);
-
-        treeView.AppendColumn("Telefono", new CellRendererText(), "text", 1);
+        treeView.AppendColumn("Teléfono", new CellRendererText(), "text", 1);
         ListStore listStore = new ListStore(typeof(String), typeof(String));
         treeView.Model = listStore;
+        listStore.AppendValues("Rosa", "777666555");
+        listStore.AppendValues("Pepe", "777666554");
+        listStore.AppendValues("Ana", "777666553");
 
-        listStore.AppendValues("Rosa", "666777555");
-        listStore.AppendValues("Pepe", "666757555");
-        listStore.AppendValues("Ana", "666757556");
+        editAction.Sensitive = false;
+
+        newAction.Activated += (sender, e) => {
+            vBoxContacto.Visible = true;
+            treeView.Sensitive = false;
+        };
+
+        newAction.Activated += (sender, e) =>{
+            vBoxContacto.Visible = true;
+            treeView.Sensitive = false;
+
+            treeView.Selection.GetSelected(out TreeIter treeIter);
+            string nombre = (string)treeView.Model.GetValue(treeIter, 0);
+            string telefono = (string)treeView.Model.GetValue(treeIter, 1);
+            Console.WriteLine("nombre={0} Telefono={1}", nombre, telefono);
+
+        };
+            treeView.Selection.Changed += (sender, e) => 
+            editAction.Sensitive = treeView.Selection.CountSelectedRows());
     }
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
